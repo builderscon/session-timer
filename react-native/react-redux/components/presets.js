352bfe47cf34f5766546c48eaaaab4a2068470bc
janus_wel <janus.wel.3@gmail.com>
2016-07-26 15:25:35 +0900

    Simplify Presets component

--- react-native/react-redux/components/presets.js
+++ react-native/react-redux/components/presets.js
@@ -2,43 +2,33 @@ import React from 'react'
 import {
     StyleSheet,
     Text,
-    TouchableWithoutFeedback,
-    View,
+    TouchableOpacity,
 } from 'react-native'
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        flexDirection: 'row',
-        justifyContent: 'flex-end',
-    },
     button: {
-        width: 180,
+        width: 140,
         height: 50,
         backgroundColor: '#444',
-        borderRadius: 36,
-        left: 36,
+        borderTopLeftRadius: 36,
+        borderBottomLeftRadius: 36,
     },
     text: {
+        height: 50,
         alignSelf: 'center',
-        fontFamily: 'avenir',
         color: '#fff',
         fontSize: 32,
-        right: 18,
-        height: 50,
+        fontFamily: 'avenir',
     },
 })
 
 export default function Presets(props) {
     return (
-        <View style={styles.container}>
-            <TouchableWithoutFeedback
-                activeOpacity={1}
-                onPress={props.onPress}>
-                <View style={styles.button}>
-                    <Text style={styles.text}>Preset</Text>
-                </View>
-            </TouchableWithoutFeedback>
-        </View>
+        <TouchableOpacity
+            onPress={props.onPress}
+            style={styles.button}
+        >
+            <Text style={styles.text}>Preset</Text>
+        </TouchableOpacity>
     )
 }
