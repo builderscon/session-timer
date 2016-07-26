e4529e3dcde89a37fbe7e73fc502178a1607ff58
janus_wel <janus.wel.3@gmail.com>
2016-07-26 15:20:56 +0900

    Simplify Logo component

--- react-native/react-redux/components/logo.js
+++ react-native/react-redux/components/logo.js
@@ -2,41 +2,31 @@ import React from 'react'
 import {
     Image,
     StyleSheet,
-    TouchableWithoutFeedback,
-    View,
+    TouchableOpacity,
 } from 'react-native'
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        flexDirection: 'row',
-        justifyContent: 'flex-start',
-    },
     button: {
-        width: 90,
         backgroundColor: '#444',
-        borderRadius: 36,
-        right: 36,
+        borderTopRightRadius: 36,
+        borderBottomRightRadius: 36,
     },
     image: {
-        alignSelf: 'center',
         width: 50,
         height: 50,
-        left: 18,
     },
 })
 
 export default function Logo(props) {
     return (
-        <View style={styles.container}>
-            <TouchableWithoutFeedback
-                activeOpacity={1}
-                onPress={props.onPress}
-            >
-                <View style={styles.button}>
-                    <Image source={{uri: 'hex_logo'}} style={styles.image} />
-                </View>
-            </TouchableWithoutFeedback>
-        </View>
+        <TouchableOpacity
+            onPress={props.onPress}
+            style={styles.button}
+        >
+            <Image
+                source={{uri: 'hex_logo'}}
+                style={styles.image}
+            />
+        </TouchableOpacity>
     )
 }
